0259492445e776de97d7026e0bf75f9b1f4ab993
Niklas Mårdby <niklas.mardby@gmail.com>
2024-09-06 13:54:26 +0200

    Added more demo code

--- public/app.js
+++ public/app.js
@@ -1,11 +1,32 @@
-console.log("Flödesövningar! F24");
+// console.log("Flödesövningar! F24");
 
-let egg = 6;
+// let egg = 6;
 
-console.log(`Du har ${egg} ägg kvar.`);
+// console.log(`Du har ${egg} ägg kvar.`);
 
-for (let i = 0; i < 6; i++) {
-	console.log(`Knäck ett ägg i skålen.`);
-	egg = egg - 1;
-	console.log(`Du har ${egg} ägg kvar.`);
+// for (let i = 0; i < 6; i++) {
+// 	console.log(`Knäck ett ägg i skålen.`);
+// 	egg = egg - 1;
+// 	console.log(`Du har ${egg} ägg kvar.`);
+// }
+
+// Övning 1
+
+// 1) Skriv ett program som skriver ut (console.log)
+// instruktioner för att skala en potatis i taget. Vi har 10 potatisar.
+
+// for (let p = 1; p <= 10; p++) {
+// 	console.log(`Skala en potatis! Potatis ${p} av 10.`);
+// }
+
+// vad är p? antal potatisar vi har skalat!
+
+const maxPotatoes = 10;
+
+for (let peeledPotatoes = 1; peeledPotatoes <= maxPotatoes; peeledPotatoes++) {
+	console.log(
+		`Skala en potatis! ${
+			maxPotatoes - peeledPotatoes
+		} potatisar kvar att skala.`
+	);
 }
